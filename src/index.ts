2dc5932bfd54519cee104f81cf413b7ef0625108
Tejas Bhatt <155186755+Tejas-PickupBiz@users.noreply.github.com>
2024-03-17 23:47:58 +0530

    fdsfs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import app from "./app";
-import connectDB from "./DB/database";
-import config from "./config/config";
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db";
+import { notFound, errorHandler } from "./middlewares/ErrorMiddleware";
+import AuthRoutes from "./routes/AuthRoutes";
 
-connectDB()
-// Example usage of Dotenv
-const PORT = process.env.PORT || 3000;
+const app: Application = express();
 
+dotenv.config();
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectDB();
+
+app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: "*",
+}));
+
+// Default
+app.get("/api", (req: Request, res: Response) => {
+  res.status(201).json({ message: "Welcome to Auth ts" });
 });
+
+// User Route
+app.use("/api/auth", AuthRoutes);
+
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (): void => console.log(`Server is running on ${PORT}`));
